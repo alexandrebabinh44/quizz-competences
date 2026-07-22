585ee3d35023e31ad51ebc425307b8d7967d501e
alexandrebabinh44 <alexandre.babinh@gmail.com>
2026-07-22 23:13:48 +0200

    Create ranking.js

---
+++ ranking.js
@@ -0,0 +1,10 @@
+const rankingState = {
+    type: "global",
+    period: "month",
+    limit: 20,
+    currentUser: null,
+    challengeIds: {
+        "challenge-x": null,
+        "challenge-y": null
+    }
+};
